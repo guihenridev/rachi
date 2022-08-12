b239b502790748cc5ed57f8c48c2f7b0ce6f429d
foskerbr <58860932+foskerbr@users.noreply.github.com>
2022-08-11 23:42:06 -0300

    Ajustando menu responsivo

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ $(window).scroll(function(event) {
     }
 
 });
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if(menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+        
+    }else{
+        menuMobile.classList.add('open');
+        
+    }
+}
